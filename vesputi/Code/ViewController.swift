0aa100100919d40d0a59c2f572e5bdddf69068c5
Vladimir Lyukov <v.lyukov@gmail.com>
2021-03-17 14:31:31 +0300

    Add MapBox SDK

--- vesputi/Code/ViewController.swift
+++ vesputi/Code/ViewController.swift
@@ -6,11 +6,18 @@
 //
 
 import UIKit
+import Mapbox
 
 class ViewController: UIViewController {
     override func loadView() {
-        view = UIView()
+        view = UIView(frame: UIScreen.main.bounds)
         view.backgroundColor = .white
+
+        let url = URL(string: "mapbox://styles/mapbox/streets-v11")
+        let mapView = MGLMapView(frame: view.bounds, styleURL: url)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mapView.setCenter(CLLocationCoordinate2D(latitude: 59.31, longitude: 18.06), zoomLevel: 9, animated: false)
+        view.addSubview(mapView)
     }
 
     override func viewDidLoad() {
