58d29056c2405fe504c3a37f8fec87732c191eef
Vladimir Lyukov <v.lyukov@gmail.com>
2021-03-17 09:53:32 +0300

    App skeleton

---
+++ vesputi/Code/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  vesputi
+//
+//  Created by Vladimir Lyukov on 17.03.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    let window = UIWindow()
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
---
+++ vesputi/Code/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vesputi
+//
+//  Created by Vladimir Lyukov on 17.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
