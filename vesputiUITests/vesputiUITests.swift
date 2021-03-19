73ef5e51c5ec907c20d287a9b4765c05686957ef
Vladimir Lyukov <v.lyukov@gmail.com>
2021-03-19 10:27:06 +0300

    Integrate swiftlint into project, fix Mint issue

--- vesputiUITests/vesputiUITests.swift
+++ vesputiUITests/vesputiUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class vesputiUITests: XCTestCase {
+class VesputiUITests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
